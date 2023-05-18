bec9e9d02b693f2e3e797bc5b3be084c78f286e4
amadediouf <diabse1@gmail.com>
2023-05-18 22:31:14 +0200

    gestion creation

--- frontend/src/app/core/services/notification.service.ts
+++ frontend/src/app/core/services/notification.service.ts
@@ -8,10 +8,11 @@ export class NotificationService extends Notyf {
   constructor() {
     super();
     this.options.position = { x: 'center', y: 'top' };
-    this.options.types = [
+   /* this.options.types = [
       {type: 'success'},
       {type: 'error',duration: 5000}
     ];
+    */
 
   }
 }

4dbbbd3c212d8cd5022478acdcdda4d6d4d8502f
a.diouf <a.diouf@cat-amania.com>
2023-07-20 22:36:33 +0200

    modification  formulaire transport et voiture

--- frontend/src/app/layout/admin-module/transporteur/transporteur.component.ts
+++ frontend/src/app/layout/admin-module/transporteur/transporteur.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {AppConfigService} from "../../../core/services/app-config.service";
+import {ActionBtn} from "../../../core/interfaces/actionBtn";
+import {Actions} from "../../../core/enum/actions";
 
 @Component({
   selector: 'app-transporteur',
@@ -9,7 +11,9 @@ import {AppConfigService} from "../../../core/services/app-config.service";
 })
 
 export class TransporteurComponent implements OnInit{
-
+  types=[{code:'S',libelle:'societe'},{code:'P',libelle:'particulier'}]
+  btns: ActionBtn[] = [];
+  titre="Creer un Nouveau  Transporteur"
   id: FormControl = new FormControl();
   prenom: FormControl = new FormControl();
   nom: FormControl = new FormControl();
@@ -33,6 +37,14 @@ export class TransporteurComponent implements OnInit{
 constructor(public builder:FormBuilder,public appConfig:AppConfigService) {
 }
   ngOnInit(): void {
+    this.initListbtns()
+    this.type.valueChanges.subscribe( (code:any)=>{
+        if(code=='S'){
+          this.prenom.disable();
+        }else {
+          this.prenom.enable();
+        }
+    });
   }
   sauvegarder(){
 
@@ -43,6 +55,43 @@ constructor(public builder:FormBuilder,public appConfig:AppConfigService) {
   annuler(){
     console.log(" annuller")
   }
+  private initListbtns() {
+    this.btns.push(new ActionBtn(this.appConfig.getLabel('dcsom.actions.enregistrer'),
+      Actions.ENREGISTRER, this.isEnrgBtnDisplayed(), true, true, true, 'save'));
+    return this.btns;
+  }
+  isEnrgBtnDisplayed(){
+    return true
+    /* this.utilisateurCourant = this.utilisateurService.getUtilisateurCourant();
+     if(!this.utilisateurCourant?.id){
+       return true
+     }*/
+    //return false
+  }
 
+  transporteurAction(event: Actions){
+    if (event === Actions.ENREGISTRER) {
+      const b= this.myform.value;
+      //this.siteService.enregistrerSite(this.myform.value).subscribe()
+    }
+  }
+  majBtnActive(){
+    this.myform?.valueChanges.subscribe((res)=>{
+      if(this.myform.invalid){
+        this.btns.forEach(b=>{
+          b.disabled=true
+        });
+      }else{
+        this.btns.forEach(b=>{
+          b.disabled=false
+        });
+      }
+    })
+    if(!this.myform.invalid){
+      this.btns.forEach(b=>{
+        b.disabled=false
+      });
+    }
+  }
 
 }
--- frontend/src/app/layout/admin-module/vehicule/vehicule.component.ts
+++ frontend/src/app/layout/admin-module/vehicule/vehicule.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {AppConfigService} from "../../../core/services/app-config.service";
 import {Transporteur} from "../../../core/interfaces/transporteur";
+import {ActionBtn} from "../../../core/interfaces/actionBtn";
+import {Actions} from "../../../core/enum/actions";
 
 @Component({
   selector: 'app-vehicule',
@@ -10,7 +12,8 @@ import {Transporteur} from "../../../core/interfaces/transporteur";
 })
 export class VehiculeComponent implements OnInit {
 
-
+  btns: ActionBtn[] = [];
+  titre="Creer un Nouveau  Vehicule"
   id: FormControl = new FormControl();
   immatriculation: FormControl = new FormControl();
   nom: FormControl = new FormControl();
@@ -28,8 +31,10 @@ export class VehiculeComponent implements OnInit {
   constructor(public builder:FormBuilder,public appConfig:AppConfigService) {
   }
   ngOnInit(): void {
+    this.initListbtns()
   }
   sauvegarder(){
+
 
   }
   reset(formToReset:string){
@@ -37,6 +42,45 @@ export class VehiculeComponent implements OnInit {
   }
   annuler(){
     console.log(" annuller")
+  }
+
+  private initListbtns() {
+    this.btns.push(new ActionBtn(this.appConfig.getLabel('dcsom.actions.enregistrer'),
+      Actions.ENREGISTRER, this.isEnrgBtnDisplayed(), true, true, true, 'save'));
+    return this.btns;
+  }
+  isEnrgBtnDisplayed(){
+    return true
+    /* this.utilisateurCourant = this.utilisateurService.getUtilisateurCourant();
+     if(!this.utilisateurCourant?.id){
+       return true
+     }*/
+    //return false
+  }
+
+  vehiculeAction(event: Actions){
+    if (event === Actions.ENREGISTRER) {
+      const b= this.myform.value;
+      //this.siteService.enregistrerSite(this.myform.value).subscribe()
+    }
+  }
+  majBtnActive(){
+    this.myform?.valueChanges.subscribe((res)=>{
+      if(this.myform.invalid){
+        this.btns.forEach(b=>{
+          b.disabled=true
+        });
+      }else{
+        this.btns.forEach(b=>{
+          b.disabled=false
+        });
+      }
+    })
+    if(!this.myform.invalid){
+      this.btns.forEach(b=>{
+        b.disabled=false
+      });
+    }
   }
 }
 

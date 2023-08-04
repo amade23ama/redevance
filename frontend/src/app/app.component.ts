1bcf807591786f41afb405e3281ae122c0258aef
a.diouf <a.diouf@cat-amania.com>
2023-08-04 22:12:44 +0200

    modification template7

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Globals} from "./app.constants";
 import {AuthService} from "./core/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,17 @@ import {AuthService} from "./core/services/auth.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  implements OnInit {
-  title = 'frontend';
   isLoggedIn = false;
-  constructor(public authService :AuthService,public globals: Globals) {
+  constructor(public authService :AuthService,public globals: Globals,public router:Router) {
     // Affichage d`un spinner lors du chargement
     globals.loading = false;
     this.authService.isLoggedIn()
   }
   ngOnInit(): void {
   this.isLoggedIn=this.authService.isLoggedIn()
+    if(!this.isLoggedIn){
+      this.router.navigate(["/login"])
+    }
     console.log(" AppComponent ")
   }
 }

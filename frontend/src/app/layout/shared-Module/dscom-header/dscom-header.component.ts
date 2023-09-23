0a3313de67ea506604924ea78b3176bd3050f883
tidiane <diome.tidiane@gmail.com>
2023-09-23 11:46:01 +0200

    suppression données initules

--- frontend/src/app/layout/shared-Module/dscom-header/dscom-header.component.ts
+++ frontend/src/app/layout/shared-Module/dscom-header/dscom-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { DroitEnum } from 'src/app/core/enum/droit-enum';
 import { Utilisateur } from 'src/app/core/interfaces/utilisateur';
 import { AppConfigService } from "../../../core/services/app-config.service";
 import { AuthService } from "../../../core/services/auth.service";
@@ -33,6 +34,22 @@ export class DscomHeaderComponent implements OnInit  {
    */
   changerPW(utilisateurConnecte: Utilisateur) {
     this.router.navigate(["admin/user/update"], {queryParams: {'contextInfo':utilisateurConnecte.id }});
+  }
+
+  /**
+   * Retour le profil de l'utilisateur connecté
+   * @returns 
+   */
+  getProfil() {
+    let role = this.authService.droits[0];
+    
+    switch (role) {
+      case DroitEnum.ADMIN: return 'Administrateur';
+      case DroitEnum.CONSULT: return 'Consultation';
+      case DroitEnum.EDIT: return 'Editeur';
+      default: return '';
+    }
+    
   }
 
 

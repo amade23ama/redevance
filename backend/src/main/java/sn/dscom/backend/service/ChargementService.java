2c785cddbd08a7e9c1ad4b1a9fa462684f8b8dc5
tidiane <diome.tidiane@gmail.com>
2023-11-11 12:19:09 +0100

    suppression

--- backend/src/main/java/sn/dscom/backend/service/ChargementService.java
+++ backend/src/main/java/sn/dscom/backend/service/ChargementService.java
@@ -737,12 +737,15 @@ public class ChargementService implements IChargementService {
             ChargementEntity chargementEntity = chargementEntityFind.get();
             if (chargementEntity.getDepots().size() == 1 || chargementEntity.getDepots().size() == 0) {
                 chargementEntity.setDepots(null);
+                this.chargementRepository.save(chargementEntity);
+                this.chargementRepository.deleteById(chargementEntity.getId());
             } else {
                 var list = chargementEntity.getDepots().subList(1, chargementEntity.getDepots().size());
                 chargementEntity.setDepots(list);
+                this.chargementRepository.save(chargementEntity);
+                //this.chargementRepository.deleteById(chargementEntity.getId());
            }
-            this.chargementRepository.save(chargementEntity);
-            this.chargementRepository.deleteById(chargementEntity.getId());
+
            return true;
         }
         return  false;

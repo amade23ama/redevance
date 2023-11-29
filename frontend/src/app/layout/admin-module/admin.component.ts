8d6b344fc2bb1089d7712c8678a95f7011a54bd2
tidiane <diome.tidiane@gmail.com>
2023-11-30 00:16:46 +0100

    correction bug

--- frontend/src/app/layout/admin-module/admin.component.ts
+++ frontend/src/app/layout/admin-module/admin.component.ts
@@ -1,7 +1,8 @@
-import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from "@angular/core";
-import {AuthService} from "../../core/services/auth.service";
-import {Globals} from "../../app.constants";
-import {Router} from "@angular/router";
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { UrlService } from "src/app/core/services/url.service";
+import { Globals } from "../../app.constants";
+import { AuthService } from "../../core/services/auth.service";
 
 @Component({
   selector: 'app-admin',
@@ -10,7 +11,7 @@ import {Router} from "@angular/router";
 })
 
 export class AdminComponent  implements OnInit , AfterViewInit{
-  constructor(public authService: AuthService,public globals: Globals,
+  constructor(public authService: AuthService,public globals: Globals, public urlService: UrlService,
               public router:Router,private cdr: ChangeDetectorRef) {
   }
   ngOnInit(): void {
